799c9071518240ecfba16bbd7b3e21919292b847
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-02-22 13:30:46 +0000

    Mon Feb 22 13:30:46 UTC 2021

--- profile_default/startup/startup.py
+++ profile_default/startup/startup.py
@@ -1,10 +1,15 @@
 try:
 	import os
-	with open(os.environ['PYTHONPREBREAK']) as pythonprebreak:
-		exec(compile(pythonprebreak.read(), os.environ['PREBREAK'], mode='exec'))
+	
+	prebreak_path = os.getenv('PYTHONPREBREAK')
+	with open(prebreak_path) as prebreak_file:
+		exec(compile(prebreak_file.read(), prebreak_path, mode='exec'))
 		# __lines = ipdb_prebreak.readlines()
 		# __i = __lines.index(next(line for line in __lines if line.startswith('import')))
 		# __j = __lines.index(next(line for line in __lines if line.startswith('builtins.')))
 		# exec(''.join(__lines[__i:__j]))
 except Exception as e:
-	print('failed running $PYTHONPREBREAK:', e.__class__.__qualname__, *e.args)
+	print('[WARN][startup.py] failed running $PYTHONPREBREAK:', e.__class__.__qualname__, *e.args)
+	
+	
+
